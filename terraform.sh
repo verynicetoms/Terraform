8abf42656b5d7c4df83af0d7db11f8a59a1d6b3b
EC2 Default User <ec2-user@ip-10-0-0-250.ap-northeast-2.compute.internal>
2023-08-16 13:23:53 +0000

    latest time is 22:23

---
+++ terraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Terraform Install Script
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum install -y terraform
+terraform version
